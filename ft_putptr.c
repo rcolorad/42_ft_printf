0214d87588d704ec77a31d10ebc7ad65bec2bab0
Ruben Colorado Hernandez <rcolorad@c1r15s4.42madrid.com>
2024-07-25 17:45:21 +0200

    correction pointer print

--- ft_putptr.c
+++ ft_putptr.c
@@ -1,17 +1,35 @@
 #include "ft_printf.h"
 
+ static int	put_hex_ptr(uintptr_t ptr)
+{
+	char	*lowercase;
+	int		num_chars;
+
+	num_chars = 0;
+	lowercase = "0123456789abcdef";
+	if (ptr >= 16)
+	{
+		num_chars += put_hex_ptr(ptr / 16);
+		num_chars += put_hex_ptr(ptr % 16);
+	}
+	else
+		num_chars += ft_putchar(lowercase[ptr]);
+	return (num_chars);
+}
+
 int	ft_putptr(void *ptr)
 {
-	int			count_chars;
-	
+	int			num_chars;
+	uintptr_t	num;
 
-	count_chars = 0;
+	num_chars = 0;
+	num = (uintptr_t)ptr;
 	if (ptr == NULL)
-		count_chars += write(1, "(nil)", 5);
-	else
 	{
-		count_chars += write(1, "0x", 2);
-		count_chars += ft_puthex((unsigned int)ptr, 1);
+		num_chars += write(1, "(nil)", 5);
+		return (num_chars);
 	}
-	return (count_chars);
+	num_chars += write(1, "0x", 2);
+	num_chars += put_hex_ptr(num);
+	return (num_chars);
 }
